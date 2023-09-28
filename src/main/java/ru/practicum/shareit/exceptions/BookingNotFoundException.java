bf6df37049ea43565122e2d43027a544b50bd7c2
Pavel Baranov <kamper.inc@yandex.ru>
2023-09-28 11:51:20 +0400

    commit 14 sprint #2 [fix-check-style]

--- src/main/java/ru/practicum/shareit/exceptions/BookingNotFoundException.java
+++ src/main/java/ru/practicum/shareit/exceptions/BookingNotFoundException.java
@@ -1,7 +1,7 @@
 package ru.practicum.shareit.exceptions;
 
-public class BookingNotFoundException extends IllegalArgumentException{
-    public BookingNotFoundException(String message){
+public class BookingNotFoundException extends IllegalArgumentException {
+    public BookingNotFoundException(String message) {
         super(message);
     }
 }
